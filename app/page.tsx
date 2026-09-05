553789843f57213f18bdfd7abc58ee06160e90d0
likescape <106062094+likescape@users.noreply.github.com>
2026-09-05 21:00:49 +0900

    Preserve mobile reference proportions in desktop previews

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,6 @@ export default function Home() {
         <div className="screen">
           <AppScreen />
         </div>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          className="device-frame-img"
-          src="/assets/mockup-frame.png"
-          alt=""
-          draggable={false}
-        />
       </div>
     </main>
   );
